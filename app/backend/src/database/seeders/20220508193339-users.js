d6f85c2abdc3c638fadaf4c885b2beedd3ebef75
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-05-09 02:05:05 -0300

    :hammer_and_wrench: add data to seeders

--- app/backend/src/database/seeders/20220508193339-users.js
+++ app/backend/src/database/seeders/20220508193339-users.js
@@ -4,15 +4,103 @@ module.exports = {
       'users',
       [
         {
-          id: 1,
-          name: 'User One',
-          email: 'user@email.com',
+          name: 'Haydon Mueller',
+          email: 'haydon@email.com',
           password: 'example_password',
         },
         {
-          id: 2,
-          name: 'User Two',
-          email: 'user@email.com',
+          name: 'Simona Spears',
+          email: 'simona@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Jacqueline Erickson',
+          email: 'jacqueline@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Bree Southern',
+          email: 'bree@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Ruqayyah Odom',
+          email: 'ruqayyah@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Brent Ferry',
+          email: 'brent@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Rhys Gallegos',
+          email: 'rhys@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Stefanie Reeves',
+          email: 'stefanie@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Uma Castillo',
+          email: 'uma@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Alima Wilkins',
+          email: 'alima@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Momina Green',
+          email: 'monina@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Dale Hatfield',
+          email: 'dale@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Albert Mill',
+          email: 'albert@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Samual Reeve',
+          email: 'samual@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Abigale Butler',
+          email: 'abigale@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Cleveland Matthews',
+          email: 'cleveland@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Jakob Buxton',
+          email: 'jakob@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Minnie Lindsay',
+          email: 'minnie@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Frances Bullock',
+          email: 'frances@email.com',
+          password: 'example_password',
+        },
+        {
+          name: 'Fox Yoder',
+          email: 'fox@email.com',
           password: 'example_password',
         },
       ],
