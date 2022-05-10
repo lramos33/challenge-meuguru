825854fe76c0c4b15807a910da30527b1626a42c
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-05-10 01:24:43 -0300

    :hammer_and_wrench: backend: add access control

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -5,6 +5,14 @@ import middlewares from './middlewares';
 const app = express();
 app.use(express.json());
 
+// for localhost request
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
+
 app.use('/users', routes.users);
 app.use(middlewares.error);
 
