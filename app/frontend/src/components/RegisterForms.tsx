1fefb2d0e8857af53f854330a9007158fd7197e1
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-05-10 16:30:02 -0300

    :sparkles: frontend: finish Register page

--- app/frontend/src/components/RegisterForms.tsx
+++ app/frontend/src/components/RegisterForms.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
+import { postUser } from '../services/api';
+
+const INITIAL_FORM_STATE = {
+  name: '',
+  email: '',
+  password: '',
+};
 
 function RegisterForm() {
-  // eslint-disable-next-line no-unused-vars
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
+  const [failedRequest, setFailedRequest] = useState(false);
+  const [successfulRequest, setSuccessfulRequest] = useState(false);
+  const [failedRequestMessage, setFailedRequestMessage] = useState('');
+  const [formData, setFormData] = useState(INITIAL_FORM_STATE);
 
-  const handleChange = (event: { target: { name: string; value: string; }; }): void => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = event.target;
 
     setFormData((prevState) => ({
@@ -17,30 +22,44 @@ function RegisterForm() {
     }));
   };
 
-  const submitForm = (event: React.MouseEvent<HTMLElement>) => {
+  const submitForm = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    // POST REQUEST HERE
+    postUser('/users', formData)
+      .then(() => {
+        setFailedRequest(false);
+        setSuccessfulRequest(true);
+        setFormData(INITIAL_FORM_STATE);
+      })
+      .catch((error) => {
+        setFailedRequest(true);
+        setSuccessfulRequest(false);
+        setFailedRequestMessage(error.response.data.error);
+      });
   };
 
   return (
-    <form>
-      <label htmlFor="name">
-        <p>Name:</p>
-        <input type="name" name="name" onChange={handleChange} />
-      </label>
+    <>
+      <form>
+        <label htmlFor="name">
+          <p>Name:</p>
+          <input type="name" name="name" value={formData.name} onChange={handleChange} />
+        </label>
 
-      <label htmlFor="email">
-        <p>Email:</p>
-        <input type="email" name="email" onChange={handleChange} />
-      </label>
+        <label htmlFor="email">
+          <p>Email:</p>
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </label>
 
-      <label htmlFor="password">
-        <p>Password:</p>
-        <input type="password" name="password" onChange={handleChange} />
-      </label>
+        <label htmlFor="password">
+          <p>Password:</p>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </label>
 
-      <button type="submit" onClick={(event) => submitForm(event)}>Submit</button>
-    </form>
+        <button type="submit" onClick={(event) => submitForm(event)}>Submit</button>
+      </form>
+      { (failedRequest) ? <p>{ failedRequestMessage }</p> : null }
+      { (successfulRequest) ? <p>User created</p> : null }
+    </>
   );
 }
 
